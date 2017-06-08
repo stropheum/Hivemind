e5ea70bbd03ce36ff6fdaac91054b15fd18eb062
stropheum <stropheum@yahoo.com>
2017-06-08 10:02:50 -0400

    commenting pass on food source

--- Hivemind/FoodSource.h
+++ Hivemind/FoodSource.h
@@ -4,30 +4,64 @@
 class FoodSource : public Entity
 {
 public:
-	/// Constructors/Destructor
+	
+#pragma region Constructors / Destructor / Move semantics
+
+	/// Constructor
+	/// @Param position: The starting position of the food source
 	explicit FoodSource(const sf::Vector2f& position);
+
+	/// Default Destructor
 	~FoodSource() = default;
 
-	/// Copy/Move Semantics
+	/// Default Copy Constructor
+	/// @Param rhs: The food source being copied
 	FoodSource(const FoodSource& rhs) = default;
+
+	/// Default Assignment Operator
+	/// @Param rhs: The food source being assigned to
+	/// @Return: A copy of the specified food source
 	FoodSource& operator=(const FoodSource& rhs) = default;
+
+	/// Default Move Copy Constructor
+	/// @Param rhs: The food source being moved
 	FoodSource(FoodSource&& rhs) = default;
+
+	/// Default Move Assignment Operator
+	/// @Param rhs: The food source being moved
+	/// @Return: A reference to the moved food source
 	FoodSource& operator=(FoodSource&& rhs) = default;
 
-	/// Public API
+#pragma endregion 
+
+#pragma region Public API
+
+	//TODO: Move update and render to Entity
 	void update(sf::RenderWindow& window, const float& deltaTime);
 	void render(sf::RenderWindow& window) const;
+
+	/// Accessor for the amount of food that is currently stored
+	/// @Return: The amount of food stored in the source
 	float getFoodAmount() const;
+
+	/// Mutator method for the food amount in the source
+	/// @Param foodAmount: The new amount of food in the source
 	void setFoodAmount(const float& foodAmount);
+
+	/// Accessor method for the center point of the food source
+	/// @Return: A vector representing the center point of the source
 	sf::Vector2f getCenterTarget() const;
+
+	/// Accessor method for the dimensions of the food source
+	/// @Return: A vector representing the width and height of the food source
 	const sf::Vector2f& getDimensions() const;
+
+#pragma endregion
 
 private:
 	/// Constants
 	const float STANDARD_WIDTH = 200.0f;
 	const float STANDARD_HEIGHT = 200.0f;
-
-	/// Private API
 
 	/// Fields
 	sf::Vector2f mDimensions;
