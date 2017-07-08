de5a54b109f62b056f3fc346e27e8d28be4d6bf6
stropheum <stropheum@yahoo.com>
2017-07-07 21:02:26 -0400

    set up all base unit tests

---
+++ Source/Hivemind.Library.Test/BeeManagerTest.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HivemindLibraryTest
+{
+	TEST_CLASS(BeeManagerTest)
+	{
+	public:
+
+		static void InitializeLeakDetection()
+		{
+			_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF);
+			_CrtMemCheckpoint(&sStartMemState);
+		}
+
+		/// Detects if memory state has been corrupted
+		static void FinalizeLeakDetection()
+		{
+			_CrtMemState endMemState, diffMemState;
+			_CrtMemCheckpoint(&endMemState);
+			if (_CrtMemDifference(&diffMemState, &sStartMemState, &endMemState))
+			{
+				_CrtMemDumpStatistics(&diffMemState);
+				Assert::Fail(L"Memory Leaks!");
+			}
+
+			// ReSharper disable CppSomeObjectMembersMightNotBeInitialized
+			UNREFERENCED_PARAMETER(endMemState);
+			UNREFERENCED_PARAMETER(diffMemState);
+			// ReSharper restore CppSomeObjectMembersMightNotBeInitialized
+		}
+
+		TEST_METHOD_INITIALIZE(methodInitialize)
+		{
+			InitializeLeakDetection();
+		}
+
+		TEST_METHOD_CLEANUP(methodCleanup)
+		{
+			FinalizeLeakDetection();
+		}
+
+		TEST_METHOD(UnitTest1)
+		{
+			Assert::Fail(L"Test not implemented");
+		}
+
+		static _CrtMemState sStartMemState;
+	};
+
+	_CrtMemState BeeManagerTest::sStartMemState;
+}
